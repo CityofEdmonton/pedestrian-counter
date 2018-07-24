39da8fec9f5f52b58994a910db7b6ca918687f84
Jared Rewerts <jaredrewerts@gmail.com>
2018-07-24 08:45:36 -0600

    Added blob detection to visualizer.

--- src/thermal_cam.py
+++ src/thermal_cam.py
@@ -6,7 +6,7 @@ import time
 
 import numpy as np
 from scipy.interpolate import griddata
-
+import cv2
 from colour import Color
 
 #low range of the sensor (this will be blue on the screen)
@@ -36,6 +36,7 @@ width = 240
 
 #the list of colors we can choose from
 blue = Color("indigo")
+detectionColor = Color("red")
 colors = list(blue.range_to(Color("red"), COLORDEPTH))
 
 #create the array of colors
@@ -53,6 +54,34 @@ pygame.mouse.set_visible(False)
 
 lcd.fill((0,0,0))
 pygame.display.update()
+
+#TODO: Remove me
+# Prepare the OpenCV blob detector. For now, this only works when saving an image.
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+
+# Change thresholds
+params.minThreshold = 10
+params.maxThreshold = 255
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 5
+
+# Filter by Circularity
+params.filterByCircularity = True
+params.minCircularity = 0.1
+
+# Filter by Convexity
+params.filterByConvexity = False
+params.minConvexity = 0.87
+
+# Filter by Inertia
+params.filterByInertia = False
+params.minInertiaRatio = 0.01
+
+# Set up the detector with default parameters.
+detector = cv2.SimpleBlobDetector_create(params)
 
 #some utility functions
 def constrain(val, min_val, max_val):
@@ -80,6 +109,21 @@ while(1):
 	pygame.display.update()
 	
 	if SAVEIMAGES:
-		pygame.image.save(pygame.display.get_surface(), "./h" + str(MAXTEMP) + "-l" + str(MINTEMP) + "_" + str(frame) + ".jpeg")
+		fileName = "./img/heatmap/h" + str(MAXTEMP) + "-l" + str(MINTEMP) + "_" + str(frame) + ".jpeg"
+		pygame.image.save(pygame.display.get_surface(), fileName)
+
+		# Read image
+        img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+        img = cv2.bitwise_not(img)
+
+		# Detect blobs.
+        keypoints = detector.detect(img)
+
+		for i in range (0, len(keypoints)):
+            x = keypoints[i].pt[0]
+            y = keypoints[i].pt[1]
+            
+			# print little circle
+			pygame.draw.circle(lcd, detectionColor, intlist(x, y), 3, 1)
 	
 	frame += 1
