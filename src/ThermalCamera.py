cb47fe30322e325adf39f4d89431fd3464a66ccd
Jared Rewerts <jaredrewerts@gmail.com>
2018-07-24 20:09:47 -0600

    Added thermal data saving.

--- src/ThermalCamera.py
+++ src/ThermalCamera.py
@@ -1,12 +1,33 @@
 from Adafruit_AMG88xx import Adafruit_AMG88xx
 from ThermalData import ThermalData
+import numpy as np
 
 
 class ThermalCamera(ThermalData):
-	def __init__(self):
-		MINTEMP = 26
-		MAXTEMP = 32
-		
+	def __init__(self, saveToDisk = False, saveLocation = "./"):
+		self.MINTEMP = 26
+		self.MAXTEMP = 32
+		#how many color values we can have
+		self.COLORDEPTH = 1024
+		self.sensor = Adafruit_AMG88xx()
+		self.saveToDisk = saveToDisk
+		self.saveLocation = saveLocation
+		self.numberOfFrames = 0
+		self.frames = []
 
-	def testFunction(self):
-		print("test")
+
+	def get(self):
+		pixels = self.sensor.readPixels()
+		pixels = [map(p, self.MINTEMP, self.MAXTEMP, 0, self.COLORDEPTH - 1) for p in pixels]
+		if self.saveToDisk:
+			self.frames.append(pixels)
+			return self.frames[-1]
+		else:
+			return pixels
+
+	def save(self):
+		if self.saveToDisk:
+			print("Writing to disk")
+			np.savetxt(self.saveLocation, self.frames)
+		else:
+			return
