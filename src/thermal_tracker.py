5a474c83ee5155ce4fdd1bc77eeb7cf48d5b8deb
Kyle Coralejo <kylecjo@gmail.com>
2018-09-12 11:11:05 -0600

    Just Formatting

--- src/thermal_tracker.py
+++ src/thermal_tracker.py
@@ -12,165 +12,175 @@ from CentroidTracker import CentroidTracker
 from multiprocessing import Process, active_children
 import pexpect
 
-#some utility functions
+# some utility functions
+
+
 def constrain(val, min_val, max_val):
-	return min(max_val, max(min_val, val))
+    return min(max_val, max(min_val, val))
+
 
 def map_value(x, in_min, in_max, out_min, out_max):
-	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-#separate process using pexpect to interact with ttn transmission
+# separate process using pexpect to interact with ttn transmission
+
+
 def transmit(str):
-	lora = pexpect.spawn('/home/pi/Projects/opencv-python/src/thethingsnetwork-send-v1')
-	while(1):
-		# handle all cases
-		i = lora.expect(['waiting', 'FAILURE', 'not sending', pexpect.TIMEOUT])
-		if i == 0:
-			lora.sendline(str)
-			print ('PedCount updated!')
-		else:
-			print ('Lora Failure: retrying...')
-			lora.terminate(force=True)
-			break
+    lora = pexpect.spawn(
+        '/home/pi/Projects/opencv-python/src/thethingsnetwork-send-v1')
+    while(1):
+        # handle all cases
+        i = lora.expect(['waiting', 'FAILURE', 'not sending', pexpect.TIMEOUT])
+        if i == 0:
+            lora.sendline(str)
+            print('PedCount updated!')
+        else:
+            print('Lora Failure: retrying...')
+            lora.terminate(force=True)
+            break
+
 
 def main():
-	MAXTEMP = 29
-	#how many color values we can have
-	COLORDEPTH = 1024
+    MAXTEMP = 29
+    # how many color values we can have
+    COLORDEPTH = 1024
 
-	# For headless pygame
-	os.putenv('SDL_VIDEODRIVER', 'dummy')
+    # For headless pygame
+    os.putenv('SDL_VIDEODRIVER', 'dummy')
 
-	# For displaying pygame
-	#os.putenv('SDL_FBDEV', '/dev/fb1')
-	pygame.init()
+    # For displaying pygame
+    #os.putenv('SDL_FBDEV', '/dev/fb1')
+    pygame.init()
 
-	#initialize the sensor
-	sensor = Adafruit_AMG88xx()
+    # initialize the sensor
+    sensor = Adafruit_AMG88xx()
 
-	points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0, 64)]
-	grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j]
+    points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0, 64)]
+    grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j]
 
-	#sensor is an 8x8 grid so lets do a square
-	height = 240
-	width = 240
+    # sensor is an 8x8 grid so lets do a square
+    height = 240
+    width = 240
 
-	#the list of colors we can choose from
-	blue = Color("indigo")
-	colors = list(blue.range_to(Color("red"), COLORDEPTH))
+    # the list of colors we can choose from
+    blue = Color("indigo")
+    colors = list(blue.range_to(Color("red"), COLORDEPTH))
 
-	#create the array of colors
-	colors = [(int(c.red * 255), int(c.green * 255), int(c.blue * 255)) for c in colors]
+    # create the array of colors
+    colors = [(int(c.red * 255), int(c.green * 255), int(c.blue * 255))
+              for c in colors]
 
-	displayPixelWidth = width / 30
-	displayPixelHeight = height / 30
+    displayPixelWidth = width / 30
+    displayPixelHeight = height / 30
 
-	lcd = pygame.display.set_mode((width, height))
+    lcd = pygame.display.set_mode((width, height))
 
-	lcd.fill((255,0,0))
+    lcd.fill((255, 0, 0))
 
-	pygame.display.update()
-	pygame.mouse.set_visible(False)
+    pygame.display.update()
+    pygame.mouse.set_visible(False)
 
-	lcd.fill((0,0,0))
-	pygame.display.update()
+    lcd.fill((0, 0, 0))
+    pygame.display.update()
 
-	# Setup SimpleBlobDetector parameters.
-	params = cv2.SimpleBlobDetector_Params()
+    # Setup SimpleBlobDetector parameters.
+    params = cv2.SimpleBlobDetector_Params()
 
-	# # Change thresholds
-	params.minThreshold = 10
-	params.maxThreshold = 255
+    # # Change thresholds
+    params.minThreshold = 10
+    params.maxThreshold = 255
 
-	# # Filter by Area.
-	params.filterByArea = True
-	params.minArea = 5
+    # # Filter by Area.
+    params.filterByArea = True
+    params.minArea = 5
 
-	# # Filter by Circularity
-	params.filterByCircularity = True
-	params.minCircularity = 0.1
+    # # Filter by Circularity
+    params.filterByCircularity = True
+    params.minCircularity = 0.1
 
-	# # Filter by Convexity
-	params.filterByConvexity = False
-	params.minConvexity = 0.87
+    # # Filter by Convexity
+    params.filterByConvexity = False
+    params.minConvexity = 0.87
 
-	# # Filter by Inertia
-	params.filterByInertia = False
-	params.minInertiaRatio = 0.01
+    # # Filter by Inertia
+    params.filterByInertia = False
+    params.minInertiaRatio = 0.01
 
-	# # Set up the detector with default parameters.
-	detector = cv2.SimpleBlobDetector_create(params)
+    # # Set up the detector with default parameters.
+    detector = cv2.SimpleBlobDetector_create(params)
 
-	#initialize centroid tracker
-	ct = CentroidTracker()
+    # initialize centroid tracker
+    ct = CentroidTracker()
 
-	#let the sensor initialize
-	time.sleep(.1)
-	frame = 0
+    # let the sensor initialize
+    time.sleep(.1)
+    frame = 0
 
-	while(True):
-		start = time.time()
-		#read the pixels
-		
-		pixels = []
-		pixels = sensor.readPixels()
-		mode_result = stats.mode([round(p) for p in pixels])
+    while(True):
+        start = time.time()
+        # read the pixels
 
-		if MAXTEMP <= mode_result[0]:
-			MAXTEMP = 37
-		pixels = [map_value(p, mode_result[0]+2, MAXTEMP, 0, COLORDEPTH - 1) for p in pixels]
+        pixels = []
+        pixels = sensor.readPixels()
+        mode_result = stats.mode([round(p) for p in pixels])
 
-		#perform interpolation
-		bicubic = griddata(points, pixels, (grid_x, grid_y), method='cubic')
+        if MAXTEMP <= mode_result[0]:
+            MAXTEMP = 37
+        pixels = [map_value(p, mode_result[0]+2, MAXTEMP, 0,
+                            COLORDEPTH - 1) for p in pixels]
 
-		#draw everything
-		for ix, row in enumerate(bicubic):
-			for jx, pixel in enumerate(row):
-				pygame.draw.rect(lcd, colors[constrain(int(pixel), 0, COLORDEPTH- 1)], (displayPixelHeight * ix, displayPixelWidth * jx, displayPixelHeight, displayPixelWidth))
-		pygame.display.update()
+        # perform interpolation
+        bicubic = griddata(points, pixels, (grid_x, grid_y), method='cubic')
 
-		surface = pygame.display.get_surface()
+        # draw everything
+        for ix, row in enumerate(bicubic):
+            for jx, pixel in enumerate(row):
+                pygame.draw.rect(lcd, colors[constrain(int(pixel), 0, COLORDEPTH - 1)],
+                                 (displayPixelHeight * ix, displayPixelWidth * jx, displayPixelHeight, displayPixelWidth))
+        pygame.display.update()
 
-		img = pygame.surfarray.array3d(surface)
-		img.swapaxes(0,1)
+        surface = pygame.display.get_surface()
 
+        img = pygame.surfarray.array3d(surface)
+        img.swapaxes(0, 1)
 
-		# Read image
-		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		img = cv2.bitwise_not(img)
-		
+        # Read image
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.bitwise_not(img)
 
-		# Detect blobs.
-		keypoints = detector.detect(img)
+        # Detect blobs.
+        keypoints = detector.detect(img)
 
-		for i in range (0, len(keypoints)):
-			x = keypoints[i].pt[0]
-			y = keypoints[i].pt[1]
+        for i in range(0, len(keypoints)):
+            x = keypoints[i].pt[0]
+            y = keypoints[i].pt[1]
 
-			# print little circle
-			pygame.draw.circle(lcd, (200, 0, 0), (int(x), int(y)), 7, 3)
-			
-		# update  our centroid tracker using the detected centroids
-		ct.update(keypoints)
+            # print little circle
+            pygame.draw.circle(lcd, (200, 0, 0), (int(x), int(y)), 7, 3)
 
-		pygame.display.update()
-		
-		frame += 1
-		time.sleep(max(1./25 - (time.time() - start), 0))
+        # update  our centroid tracker using the detected centroids
+        ct.update(keypoints)
 
-		# transmit pedcount data every 100 frames
-		if frame == 1 or frame % 100 == 0:
-			# end current lora processes
-			plist = active_children()
-			for p in plist:
-				if p.name == 'lora_proc':
-					p.terminate()
-			loraproc = Process(target=transmit,  name='lora_proc', args = (str(ct.get_count()),))
-			loraproc.start()
-		
-		print(ct.get_count())
+        pygame.display.update()
 
-	print("terminating...")
+        frame += 1
+        time.sleep(max(1./25 - (time.time() - start), 0))
+
+        # transmit pedcount data every 100 frames
+        if frame == 1 or frame % 100 == 0:
+            # end current lora processes
+            plist = active_children()
+            for p in plist:
+                if p.name == 'lora_proc':
+                    p.terminate()
+            loraproc = Process(
+                target=transmit,  name='lora_proc', args=(str(ct.get_count()),))
+            loraproc.start()
+
+        print(ct.get_count())
+
+    print("terminating...")
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- src/thethingsnetwork-send-v1.cpp
+++ src/thethingsnetwork-send-v1.cpp
@@ -32,137 +32,148 @@
 
 // LoRaWAN Application identifier (AppEUI)
 // Not used in this example
-static const u1_t APPEUI[8]  = { 0x02, 0x00, 0x00, 0x00, 0x00, 0xEE, 0xFF, 0xC0 };
+static const u1_t APPEUI[8] = {0x02, 0x00, 0x00, 0x00, 0x00, 0xEE, 0xFF, 0xC0};
 
 // LoRaWAN DevEUI, unique device ID (LSBF)
 // Not used in this example
-static const u1_t DEVEUI[8]  = { 0x42, 0x42, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF };
+static const u1_t DEVEUI[8] = {0x42, 0x42, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
 
-// LoRaWAN NwkSKey, network session key 
+// LoRaWAN NwkSKey, network session key
 // Use this key for The Things Network
-static const u1_t DEVKEY[16] = { 0x8A, 0x2A, 0xD0, 0xE6, 0xBE, 0x4F, 0x77, 0x58, 0xD3, 0xB1, 0xB6, 0x04, 0x03, 0xD4, 0xAB, 0xAA };
+static const u1_t DEVKEY[16] = {0x8A, 0x2A, 0xD0, 0xE6, 0xBE, 0x4F, 0x77, 0x58, 0xD3, 0xB1, 0xB6, 0x04, 0x03, 0xD4, 0xAB, 0xAA};
 
 // LoRaWAN AppSKey, application session key
 // Use this key to get your data decrypted by The Things Network
-static const u1_t ARTKEY[16] = { 0xBF, 0x43, 0xCA, 0x95, 0x98, 0x38, 0x54, 0xC1, 0xA6, 0x8F, 0xA0, 0x46, 0xAD, 0x04, 0xF3, 0xC5 };
+static const u1_t ARTKEY[16] = {0xBF, 0x43, 0xCA, 0x95, 0x98, 0x38, 0x54, 0xC1, 0xA6, 0x8F, 0xA0, 0x46, 0xAD, 0x04, 0xF3, 0xC5};
 
 // LoRaWAN end-device address (DevAddr)
 // See http://thethingsnetwork.org/wiki/AddressSpace
-static const u4_t DEVADDR = 0x26021CD9 ; // <-- Change this address for every node!
+static const u4_t DEVADDR = 0x26021CD9; // <-- Change this address for every node!
 
 //////////////////////////////////////////////////
 // APPLICATION CALLBACKS
 //////////////////////////////////////////////////
 
 // provide application router ID (8 bytes, LSBF)
-void os_getArtEui (u1_t* buf) {
+void os_getArtEui(u1_t *buf)
+{
     memcpy(buf, APPEUI, 8);
 }
 
 // provide device ID (8 bytes, LSBF)
-void os_getDevEui (u1_t* buf) {
+void os_getDevEui(u1_t *buf)
+{
     memcpy(buf, DEVEUI, 8);
 }
 
 // provide device key (16 bytes)
-void os_getDevKey (u1_t* buf) {
+void os_getDevKey(u1_t *buf)
+{
     memcpy(buf, DEVKEY, 16);
 }
 
-u4_t cntr=0;
+u4_t cntr = 0;
 u1_t mydata[] = {"    "};
 static osjob_t sendjob;
 
 // Pin mapping
 lmic_pinmap pins = {
-  .nss = 6,
-  .rxtx = UNUSED_PIN, // Not connected on RFM92/RFM95
-  .rst = 0,  // Needed on RFM92/RFM95
-  .dio = {7,4,5}
-};
+    .nss = 6,
+    .rxtx = UNUSED_PIN, // Not connected on RFM92/RFM95
+    .rst = 0,           // Needed on RFM92/RFM95
+    .dio = {7, 4, 5}};
 
-
-void onEvent (ev_t ev) {
+void onEvent(ev_t ev)
+{
     //debug_event(ev);
-    switch(ev) {
-      // scheduled data sent (optionally data received)
-      // note: this includes the receive window!
-      case EV_TXCOMPLETE:
-          // use this event to keep track of actual transmissions
-          fprintf(stdout, "Event EV_TXCOMPLETE, time: %d\n", millis() / 1000);
-//	  fwrite(&mydata, 1, 5, stdout);
-//	  fflush(stdout);
-          if(LMIC.dataLen) { // data received in rx slot after tx
-              //debug_buf(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
-              fprintf(stdout, "Data Received!\n");
-          }
-          break;
-       default:
-          break;
+    switch (ev)
+    {
+    // scheduled data sent (optionally data received)
+    // note: this includes the receive window!
+    case EV_TXCOMPLETE:
+        // use this event to keep track of actual transmissions
+        fprintf(stdout, "Event EV_TXCOMPLETE, time: %d\n", millis() / 1000);
+        //	  fwrite(&mydata, 1, 5, stdout);
+        //	  fflush(stdout);
+        if (LMIC.dataLen)
+        { // data received in rx slot after tx
+            //debug_buf(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
+            fprintf(stdout, "Data Received!\n");
+        }
+        break;
+    default:
+        break;
     }
 }
 
 // used to read data from python program
-void readData(){
+void readData()
+{
     char buf[8];
     fprintf(stdout, "waiting");
-    fscanf(stdin,"%s",&buf);
-    int i=0;
-    while(buf[i]){
-	mydata[i] = buf[i];
- 	i++;
+    fscanf(stdin, "%s", &buf);
+    int i = 0;
+    while (buf[i])
+    {
+        mydata[i] = buf[i];
+        i++;
     }
 }
 
-static void do_send(osjob_t* j){
-      time_t t=time(NULL);
-      fprintf(stdout, "[%x] (%ld) %s\n", hal_ticks(), t, ctime(&t));
-      // Show TX channel (channel numbers are local to LMIC)
-      // Check if there is not a current TX/RX job running
-    if (LMIC.opmode & (1 << 7)) {
-      fprintf(stdout, "OP_TXRXPEND, not sending");
-    } else {
-      readData();
-      LMIC_setTxData2(1,  mydata, sizeof(mydata), 0);
+static void do_send(osjob_t *j)
+{
+    time_t t = time(NULL);
+    fprintf(stdout, "[%x] (%ld) %s\n", hal_ticks(), t, ctime(&t));
+    // Show TX channel (channel numbers are local to LMIC)
+    // Check if there is not a current TX/RX job running
+    if (LMIC.opmode & (1 << 7))
+    {
+        fprintf(stdout, "OP_TXRXPEND, not sending");
+    }
+    else
+    {
+        readData();
+        LMIC_setTxData2(1, mydata, sizeof(mydata), 0);
     }
     // Schedule a timed job to run at the given timestamp (absolute system time)
-    os_setTimedCallback(j, os_getTime()+sec2osticks(15), do_send);
-         
+    os_setTimedCallback(j, os_getTime() + sec2osticks(15), do_send);
 }
 
-
-void setup() {
-  // LMIC init
-  wiringPiSetup();
-  os_init();
-  // Reset the MAC state. Session and pending data transfers will be discarded.
-  LMIC_reset();
-  // Set static session parameters. Instead of dynamically establishing a session 
-  // by joining the network, precomputed session parameters are be provided.
-  LMIC_setSession (0x1, DEVADDR, (u1_t*)DEVKEY, (u1_t*)ARTKEY);
-  // Set up TTN channels
-  LMIC_selectSubBand(1);
-  // Disable data rate adaptation
-  LMIC_setAdrMode(0);
-  // Disable link check validation
-  LMIC_setLinkCheckMode(0);
-  // Disable beacon tracking
-  LMIC_disableTracking ();
-  // Stop listening for downstream data (periodical reception)
-  LMIC_stopPingable();
-  // Set data rate and transmit power (note: txpow seems to be ignored by the library)  LMIC_setDrTxpow(DR_SF7,14);
-  // TTN uses SF9 for its RX2 window
-  LMIC.dn2Dr = DR_SF9;
+void setup()
+{
+    // LMIC init
+    wiringPiSetup();
+    os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+    // Set static session parameters. Instead of dynamically establishing a session
+    // by joining the network, precomputed session parameters are be provided.
+    LMIC_setSession(0x1, DEVADDR, (u1_t *)DEVKEY, (u1_t *)ARTKEY);
+    // Set up TTN channels
+    LMIC_selectSubBand(1);
+    // Disable data rate adaptation
+    LMIC_setAdrMode(0);
+    // Disable link check validation
+    LMIC_setLinkCheckMode(0);
+    // Disable beacon tracking
+    LMIC_disableTracking();
+    // Stop listening for downstream data (periodical reception)
+    LMIC_stopPingable();
+    // Set data rate and transmit power (note: txpow seems to be ignored by the library)  LMIC_setDrTxpow(DR_SF7,14);
+    // TTN uses SF9 for its RX2 window
+    LMIC.dn2Dr = DR_SF9;
 }
 
-int main() {
-  
-  setup();
-  do_send(&sendjob);
-  while(1){
-    os_runloop();
-  }
-  return 0;
+int main()
+{
+
+    setup();
+    do_send(&sendjob);
+    while (1)
+    {
+        os_runloop();
+    }
+    return 0;
 }
 
 //extern "C" {
