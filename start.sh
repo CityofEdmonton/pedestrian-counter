0c82702518b3d3e099d6c41b345ce449c8a12887
Kyle Coralejo <kylecjo@gmail.com>
2018-09-19 11:31:52 -0600

    Changed hardcoded filepaths in bash script

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 echo "Activating the virtual python environment"
-source /home/pi/Projects/opencv-python/ped-detector/bin/activate
+source $DIR/ped-detector/bin/activate #ped-detector is the name of your virtual env
+
+
 echo "Running the pedestrian detector"
-python /home/pi/Projects/opencv-python/src/thermal_tracker.py
+python $DIR/src/thermal_tracker.py
 
