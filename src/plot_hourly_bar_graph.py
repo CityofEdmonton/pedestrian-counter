b5839e374ff9ffadd6337eff241912f657a440dc
Liyao Jiang <liyao1@ualberta.ca>
2019-04-01 12:14:48 -0600

    automate bar graph ploting script

--- src/plot_hourly_bar_graph.py
+++ src/plot_hourly_bar_graph.py
@@ -3,38 +3,70 @@ from dateutil.parser import parse
 import datetime
 import csv
 import numpy
+import os
+import copy
+
+directory = r"..\graphs\hourly_bar_graph"
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
 list = []
-x = []
-y = []
-bins_x = []
 
 with open('../data/data.csv', 'r') as csvfile:
-    plots = csv.reader((l.replace('\0', '') for l in csvfile))
+    plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         try:
             list.append([datetime.datetime.strptime(
-                row[0], '%m/%d/%y %H:%M'), int(row[1])])
+                row[0], '%Y-%m-%d %H:%M:%S.%f'), int(row[1])])
         except:
             pass
 
-bins_x = []
-bins_y = []
+bins_x = set()
+date_dict = {}
+start_count = None
+start_date = None
 
 for element in list:
     if not element[0] in bins_x:
-        bins_x.append(element[0])
-        bins_y.append(0)
+        bins_x.add(element[0])
+        if start_date == element[0].date():
+            date_dict[start_date][0].append(element[0])
+            date_dict[start_date][1].append(element[1]-start_count)
+        else:
+            # set the new date and count as the base
+            start_date = element[0].date()
+            start_count = element[1]
+            date_dict[start_date] = ([],[])
+            date_dict[start_date][0].append(element[0])
+            date_dict[start_date][1].append(element[1]-start_count)
 
-for i in range(0, len(list)):
-    bin_index = bins_x.index(list[i][0])
-    bins_y[bin_index] += list[i][1]
+# calculate the non-cumulative data by taking the difference
+date_dict_noncumu = copy.deepcopy(date_dict)
+for key, value in date_dict.items():
+    for i in range(1,len(value[1])):
+        date_dict_noncumu[key][1][i] = date_dict[key][1][i] - date_dict[key][1][i-1]
 
-bins_hours = [element.hour for element in bins_x]
-
-plt.bar(bins_hours, bins_y)
-plt.xlabel('Hour') # x-axis label
-plt.ylabel('Count') # y-axis label
-plt.xticks(bins_hours, fontsize=10)
-plt.title('Date') # plot title
-plt.show()
+for key, value in date_dict_noncumu.items():
+    # add up the counts into hourly bins
+    bins_hours = set()
+    bins_value = dict()
+    for i in range(0,len(value[0])):
+        h = value[0][i].hour
+        bins_hours.add(h)
+        if h not in bins_value.keys():
+            bins_value[h] = value[1][i]
+        else:
+            bins_value[h] += value[1][i]
+    bins_hours = sorted(bins_hours)
+    bins_value = [x[1] for x in sorted(bins_value.items())]
+    
+    # plot the daily graph
+    plt.bar(bins_hours, bins_value) 
+    plt.xlabel('Hour') # x-axis label
+    plt.ylabel('Count') # y-axis label
+    plt.xticks(bins_hours, fontsize=10)
+    plt.title('Date: ' + str(key)) # plot title
+    figure = plt.gcf() # get current figure
+    figure.set_size_inches(8, 6)
+    plt.savefig(r"%s\%s.png"%(directory, str(key)), dpi=200)
+    plt.close()
