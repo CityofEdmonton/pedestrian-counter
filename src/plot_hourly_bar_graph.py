c27e8f9441d5255eeac3eaa0810521d0b0383a92
LiyaoJiang1998 <liyao1@ualberta.ca>
2019-04-03 11:57:36 -0600

    foramting code

--- src/plot_hourly_bar_graph.py
+++ src/plot_hourly_bar_graph.py
@@ -17,20 +17,20 @@ class CountReading:
 # plotting params
 params = {'legend.fontsize': 'x-large',
           'figure.figsize': (12, 9),
-         'axes.labelsize': 'x-large',
-         'axes.titlesize':'x-large',
-         'xtick.labelsize':'x-large',
-         'ytick.labelsize':'x-large'}
+          'axes.labelsize': 'x-large',
+          'axes.titlesize': 'x-large',
+          'xtick.labelsize': 'x-large',
+          'ytick.labelsize': 'x-large'}
 pylab.rcParams.update(params)
 
-directory = os.path.join('..','graphs')
-directory = os.path.join(directory,'hourly_bar_graph')
+directory = os.path.join('..', 'graphs')
+directory = os.path.join(directory, 'hourly_bar_graph')
 if not os.path.exists(directory):
     os.makedirs(directory)
 
 data = []
 
-with open(os.path.join(os.path.join('..','data'), 'data.csv'), 'r') as csvfile:
+with open(os.path.join(os.path.join('..', 'data'), 'data.csv'), 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         try:
@@ -55,16 +55,17 @@ for timestamp, count in data:
 # calculate the non-cumulative data by taking the difference
 date_to_count_reading_noncumu = copy.deepcopy(date_to_count_reading)
 for date, count_reading_list in date_to_count_reading.items():
-    for i in range(1,len(count_reading_list)):
+    for i in range(1, len(count_reading_list)):
         # timestamps = [count_reading.timestamp for count_reading in count_reading_list]
         # counts = [count_reading.count for count_reading in count_reading_list]
-        date_to_count_reading_noncumu[date][i].count = date_to_count_reading[date][i].count - date_to_count_reading[date][i-1].count
+        date_to_count_reading_noncumu[date][i].count = date_to_count_reading[date][i].count - \
+            date_to_count_reading[date][i-1].count
 
 for date, count_reading_list in date_to_count_reading_noncumu.items():
     # add up the counts into hourly bins
     bins_hours = set()
     hour_to_total_count = dict()
-    for i in range(0,len(count_reading_list)):
+    for i in range(0, len(count_reading_list)):
         h = count_reading_list[i].timestamp.hour
         bins_hours.add(h)
         if h not in hour_to_total_count.keys():
@@ -72,15 +73,16 @@ for date, count_reading_list in date_to_count_reading_noncumu.items():
         else:
             hour_to_total_count[h] += count_reading_list[i].count
     bins_hours = sorted(bins_hours)
-    total_count_list = [count for hour, count in sorted(hour_to_total_count.items())]
-    
+    total_count_list = [count for hour,
+                        count in sorted(hour_to_total_count.items())]
+
     # plot the daily graph
     plt.bar(bins_hours, total_count_list)
-    plt.xlabel('Hour') # x-axis label
-    plt.ylabel('Count') # y-axis label
+    plt.xlabel('Hour')  # x-axis label
+    plt.ylabel('Count')  # y-axis label
     plt.xticks(bins_hours, fontsize=10)
-    plt.title('Date: ' + str(date)) # plot title
-    figure = plt.gcf() # get current figure
+    plt.title('Date: ' + str(date))  # plot title
+    figure = plt.gcf()  # get current figure
     figure.set_size_inches(8, 6)
-    plt.savefig(os.path.join(directory,"%s.png"%str(date)), dpi=200)
+    plt.savefig(os.path.join(directory, "%s.png" % str(date)), dpi=200)
     plt.close()
