3f79f64d3aaa4f122be23a7ea3c138bb6e3c0ffb
Liyao Jiang <liyao1@ualberta.ca>
2019-05-27 15:41:02 -0600

    reformat lora mode  to read params as arguments

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+
 if [ -z "$1" ]; then
     echo "Pass virtual env directory as argument"
 else
@@ -8,13 +9,27 @@ else
     source $DIR/$1/bin/activate #ped-detector is the name of your virtual env
     echo "Running the pedestrian detector"
     if [ "$2" == "--headless" ]; then
-        python $DIR/src/thermal_tracker.py 256 --headless
+        python $DIR/src/thermal_tracker.py --headless \
+        --color_depth=256 \
+        --max_temp=31 \
+        --ambient_offset=4 \
+        --ambient_time=120 \
+        --blob_min_threshold=0 \
+        --blob_filterbyarea --blob_min_area=750 \
+        --lora_send_interval=30
     else 
-        python $DIR/src/thermal_tracker.py  256
+        python $DIR/src/thermal_tracker.py \
+        --color_depth=256 \
+        --max_temp=31 \
+        --ambient_offset=4 \
+        --ambient_time=120 \
+        --blob_min_threshold=0 \
+        --blob_filterbyarea --blob_min_area=750 \
+        --lora_send_interval=30
     fi
 fi
 
-
-
-
-
+        # --blob_filterbycircularity --blob_min_circularity=0.1 \
+        # --blob_filterbyconvexity --blob_min_convexity=0.87 \
+        # --blob_filterbyinertia --blob_min_inertiaratio=0.01 \
+        # --blob_max_threshold=255 \
